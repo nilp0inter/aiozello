5722b970b55b6951f1a649d9eaae9308e6f9d00e
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-08-24 23:18:27 +0200

    refactor: black & docs

--- aiozello/auth.py
+++ aiozello/auth.py
@@ -1,24 +1,48 @@
 import time
+from typing import Callable
 
 import jwt
 
 
-def generate_token(issuer, private_key, expiration=3600, current_time=lambda: int(time.time())):
+def current_time() -> int:
+    """An effectful function that returns the current time."""
+    return int(time.time())
+
+
+def generate_token(
+    issuer: str,
+    private_key: str,
+    expiration: int,
+    get_current_time: Callable[[], int],
+):
+    """A pure function that generates a JWT token."""
     payload = {
         "iss": issuer,
-        "exp": current_time() + expiration,
+        "exp": get_current_time() + expiration,
     }
 
     return jwt.encode(payload, private_key, algorithm="RS256")
 
 
 class LocalTokenManager:
-    def __init__(self, issuer, private_key_path, expiration=3600, current_time=lambda: int(time.time())):
+    """A class that generates a JWT token from a local private key."""
+    def __init__(
+        self,
+        issuer: str,
+        private_key_path: str,
+        expiration: int = 3600,
+        get_current_time: Callable[[], int] = current_time,
+    ):
         self.issuer = issuer
         with open(private_key_path, "r") as f:
             self.private_key = f.read()
         self.expiration = expiration
-        self.current_time = current_time
+        self.get_current_time = get_current_time
 
     def issue(self):
-        return generate_token(self.issuer, self.private_key, expiration=self.expiration, current_time=self.current_time)
+        return generate_token(
+            self.issuer,
+            self.private_key,
+            expiration=self.expiration,
+            get_current_time=self.get_current_time,
+        )
