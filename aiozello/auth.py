38aab2c632e9adec581ee886b205b26e4ff6fd82
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-08-25 01:40:17 +0200

    doc: make function names more explicit

--- aiozello/auth.py
+++ aiozello/auth.py
@@ -11,7 +11,6 @@ def generate_token_payload(
     current_time: int,
     expiration: int,
 ):
-    """A pure function that generates a JWT payload."""
     return {
         "iss": issuer,
         "exp": current_time + expiration,
@@ -24,7 +23,6 @@ def generate_token(
     current_time: int,
     expiration: int,
 ):
-    """A function that generates a JWT token."""
     payload = generate_token_payload(issuer, current_time, expiration)
 
     try:
@@ -33,30 +31,28 @@ def generate_token(
         raise TokenGenerationError("Failed to generate token") from e
 
 
-def get_current_time() -> int:
-    """A function that returns the current time."""
+def get_system_current_time() -> int:
     return int(time.time())
 
 
-def read_private_key(path: str) -> str:
-    """A function that reads a private key from a file."""
+def read_private_key_from_file(path: str) -> str:
     with open(path, "r") as f:
         return f.read()
 
 
 class LocalTokenManager:
-    """A class that generates a JWT token from a local private key."""
+    """A class that generates a JWT token from private key stored in a local file."""
 
     def __init__(
         self,
         issuer: str,
         private_key_path: str,
-        read_private_key: Callable[[str], str] = read_private_key,
-        get_current_time: Callable[[], int] = get_current_time,
+        get_private_key: Callable[[str], str] = read_private_key_from_file,
+        get_current_time: Callable[[], int] = get_system_current_time,
         expiration: int = 3600,
     ):
         self.issuer = issuer
-        self.private_key = read_private_key(private_key_path)
+        self.private_key = get_private_key(private_key_path)
         self.get_current_time = get_current_time
         self.expiration = expiration
 
