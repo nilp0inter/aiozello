67b4669aaafd593124004da76d8af7fa0dc34676
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-08-25 00:49:04 +0200

    refactor: remove get_current_time from generate_token

--- aiozello/auth.py
+++ aiozello/auth.py
@@ -7,13 +7,13 @@ import jwt
 def generate_token(
     issuer: str,
     private_key: str,
+    current_time: int,
     expiration: int,
-    get_current_time: Callable[[], int],
 ):
     """A pure function that generates a JWT token."""
     payload = {
         "iss": issuer,
-        "exp": get_current_time() + expiration,
+        "exp": current_time + expiration,
     }
 
     return jwt.encode(payload, private_key, algorithm="RS256")
@@ -38,18 +38,18 @@ class LocalTokenManager:
         issuer: str,
         private_key_path: str,
         read_private_key: Callable[[str], str] = read_private_key,
-        expiration: int = 3600,
         get_current_time: Callable[[], int] = get_current_time,
+        expiration: int = 3600,
     ):
         self.issuer = issuer
         self.private_key = read_private_key(private_key_path)
-        self.expiration = expiration
         self.get_current_time = get_current_time
+        self.expiration = expiration
 
     def issue(self):
         return generate_token(
             self.issuer,
             self.private_key,
-            expiration=self.expiration,
-            get_current_time=self.get_current_time,
+            self.get_current_time(),
+            self.expiration,
         )
