81fd59c52f4a7a491aaeab9359eb0502304131c8
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-08-25 00:13:22 +0200

    refactor: separate effectful functions from pure functions

--- aiozello/auth.py
+++ aiozello/auth.py
@@ -4,11 +4,6 @@ from typing import Callable
 import jwt
 
 
-def get_current_time() -> int:
-    """An effectful function that returns the current time."""
-    return int(time.time())
-
-
 def generate_token(
     issuer: str,
     private_key: str,
@@ -24,18 +19,30 @@ def generate_token(
     return jwt.encode(payload, private_key, algorithm="RS256")
 
 
+def get_current_time() -> int:
+    """An effectful function that returns the current time."""
+    return int(time.time())
+
+
+def read_private_key(path: str) -> str:
+    """An effectful function that reads a private key from a file."""
+    with open(path, "r") as f:
+        return f.read()
+
+
 class LocalTokenManager:
     """A class that generates a JWT token from a local private key."""
+
     def __init__(
         self,
         issuer: str,
         private_key_path: str,
+        read_private_key: Callable[[str], str] = read_private_key,
         expiration: int = 3600,
         get_current_time: Callable[[], int] = get_current_time,
     ):
         self.issuer = issuer
-        with open(private_key_path, "r") as f:
-            self.private_key = f.read()
+        self.private_key = read_private_key(private_key_path)
         self.expiration = expiration
         self.get_current_time = get_current_time
 
