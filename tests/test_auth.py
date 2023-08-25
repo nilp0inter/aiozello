2b808f05706ff2d22b92711e050b871955a7b8a1
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-08-25 02:20:52 +0200

    refactor: clean test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,12 +7,16 @@ from aiozello.auth import LocalTokenManager, get_system_current_time
 
 
 def test_LocalTokenManager_happy_path():
-    with (tempfile.NamedTemporaryFile(suffix=".key") as private_key_file,
-          tempfile.NamedTemporaryFile(suffix=".pem") as public_key_file):
+    with (
+        tempfile.NamedTemporaryFile(suffix=".key") as private_key_file,
+        tempfile.NamedTemporaryFile(suffix=".pem") as public_key_file,
+    ):
         # 1. Create a pair of RSA private/public keys
-        private_key = cryptography.hazmat.primitives.asymmetric.rsa.generate_private_key(
-            public_exponent=65537,
-            key_size=2048,
+        private_key = (
+            cryptography.hazmat.primitives.asymmetric.rsa.generate_private_key(
+                public_exponent=65537,
+                key_size=2048,
+            )
         )
         private_key_pem = private_key.private_bytes(
             encoding=cryptography.hazmat.primitives.serialization.Encoding.PEM,
@@ -33,16 +37,18 @@ def test_LocalTokenManager_happy_path():
         public_key_file.flush()
 
         # 3. Create a LocalTokenManager instance
+        issuer = "foobar"
         current_time = get_system_current_time()
         expiration = 42
+
         ltm = LocalTokenManager(
-            issuer="test",
+            issuer=issuer,
             private_key_path=private_key_file.name,
             expiration=expiration,
             get_current_time=lambda: current_time,
         )
 
-        # 4. Call issue() method
+        # 4. Issue a token
         token = ltm.issue()
 
         # 5. Verify that the returned token is valid using jwt.decode()
@@ -55,5 +61,5 @@ def test_LocalTokenManager_happy_path():
         except Exception as e:
             raise AssertionError("Failed to decode the token") from e
         else:
-            assert decoded_token["iss"] == "test"
+            assert decoded_token["iss"] == issuer
             assert decoded_token["exp"] == current_time + expiration
